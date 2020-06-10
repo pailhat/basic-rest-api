947a9a55be5b8e12a759a524deb1b43b485ddf9c
Paul <prevelo01@manhattan.edu>
2020-06-10 16:42:38 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+//ROUTES
+app.get('/', (req,res) => {
+    res.send('Sup we are on the home page.');
+});
+
+
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        })
+        .then(() => console.log('DB Connected!'))
+        .catch(err => {
+        console.log(err);
+});
+
+//How do we start listening to the server?
+app.listen(3000);
